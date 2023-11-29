04796a77e339b58910e8ee47b265a72b12799758
DESKTOP-KU09SPM\Ezgi <ezgi.ozgur@mepsan.com.tr>
2023-11-29 17:22:52 +0300

    added models

---
+++ db_transaction/models/default_communication_model.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column
+from sqlalchemy.types import String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class DefaultCommunication(Base):
+    __tablename__ = 'DefaultCommunication'
+
+    csrf_token = Column(String)
+    ethernet_ip_1 = Column(String)
+    ethernet_ip_2 = Column(String)
+    ethernet_port_1 = Column(String)
+    ethernet_port_2 = Column(String)
+    gprs_ip_1 = Column(String)
+    gprs_ip_2 = Column(String)
+    gprs_port_1 = Column(String)
+    gprs_port_2 = Column(String)
+    ntp_ip_1 = Column(String)
+    ntp_ip_2 = Column(String)
+    ntp_port_1 = Column(String)
+    ntp_port_2 = Column(String)
+    ssh_ethernet_ip_1 = Column(String)
+    ssh_ethernet_ip_2 = Column(String)
+    ssh_ethernet_port_1 = Column(String)
+    ssh_ethernet_port_2 = Column(String)
+    created_date = Column(DateTime)
